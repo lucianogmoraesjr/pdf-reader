bd5ca442b4e3b07976933767df8d77fe8d55a51f
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2024-10-21 19:46:28 -0300

    feat: verify if pdf is encrypted

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,33 @@
-import { Recipe } from 'muhammara';
+import { Recipe, createReader } from 'muhammara';
 import path from 'node:path';
 
 async function createPdfWithSignatureProof(originalPdfPath: string, signatureProofPath: string, outputPdfPath: string) {
+  const pdfReader = createReader(originalPdfPath)
+  const isEncrypted = pdfReader.isEncrypted()
+
+  if (isEncrypted) {
+    console.log('PDF encriptado')
+  }
+
+  const totalPages = pdfReader.getPagesCount()
+
   const pdfDoc = new Recipe(originalPdfPath, outputPdfPath)
 
+  for (let page = 1; page <= totalPages; page++) {
+    pdfDoc
+      .editPage(page)
+      .text("Add footer", 150, 800, {
+        color: "#000000",
+      })
+      .endPage()
+  }
+
   pdfDoc
-    .editPage(1)
-    .text("Add some texts to an existing pdf file", 150, 300)
-    .endPage()
     .appendPage(signatureProofPath)
     .endPDF();
 }
 
-const originalPdfPath = path.resolve(__dirname, 'test-protected.pdf');
+const originalPdfPath = path.resolve(__dirname, 'test-two-pages-protected-with-header.pdf');
 const signatureProofPath = path.resolve(__dirname, 'proof.pdf');
 const outputPdfPath = path.resolve(__dirname, 'signed-document.pdf');
 
